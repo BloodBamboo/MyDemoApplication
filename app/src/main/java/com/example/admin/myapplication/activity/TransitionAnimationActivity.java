2765606a5a8e40ce7d12f1ae42de7c9049f167ac
yangx <yangx@edu-edu.com.cn>
2017-05-31 09:31:30 +0800

    调整调整首页按钮排版添加滚动

---
+++ app/src/main/java/com/example/admin/myapplication/activity/TransitionAnimationActivity.java
@@ -0,0 +1,10 @@
+package com.example.admin.myapplication.activity;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administrator on 2017/5/31.
+ */
+
+public class TransitionAnimationActivity extends AppCompatActivity {
+}
