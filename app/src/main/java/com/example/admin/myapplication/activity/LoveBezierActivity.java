96b300b11c3602730607117c6c403cabbb7f90aa
yangx <yangx@edu-edu.com.cn>
2017-06-16 16:59:59 +0800

    完善打赏例子

--- app/src/main/java/com/example/admin/myapplication/activity/LoveBezierActivity.java
+++ app/src/main/java/com/example/admin/myapplication/activity/LoveBezierActivity.java
@@ -8,20 +8,24 @@ import android.view.View;
 import android.widget.Toast;
 
 import com.example.admin.myapplication.R;
+import com.example.admin.myapplication.view.ui.LoveLayout;
 
 /**
  * Created by Administrator on 2017/6/15.
  */
 
 public class LoveBezierActivity extends AppCompatActivity{
+    LoveLayout loveLayout;
+
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_love_bezier);
+        loveLayout = (LoveLayout) findViewById(R.id.iv_love);
     }
 
     public void onClick(View view) {
-        Toast.makeText(this, "=========", Toast.LENGTH_SHORT).show();
+        loveLayout.addLoveIcon();
     }
 }
