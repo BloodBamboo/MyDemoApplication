16e6fa1067f080592e2bad07222e2c4318c08fb0
yangx <yangx@edu-edu.com.cn>
2019-12-03 10:28:24 +0800

    升级gradle版本配合ndkr15版本

---
+++ app/src/main/java/com/example/admin/myapplication/Utils/TwoTuple.java
@@ -0,0 +1,11 @@
+package com.example.admin.myapplication.Utils;
+
+public class TwoTuple<T, K> {
+    public T one;
+    public K two;
+
+    public TwoTuple(T t, K k) {
+        this.one = t;
+        this.two = k;
+    }
+}
